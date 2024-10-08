b1122d35e24fd0d951bfd6eb48f17b96291184c7
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 17:42:05 +0800

    modified files for better responsiveness

--- first-portfolio/src/components/Container.tsx
+++ first-portfolio/src/components/Container.tsx
@@ -6,6 +6,6 @@ type ContainerProps = {
 
 export const Container: React.FC<ContainerProps> = ({children}) => {
     return(
-        <div className="bg-black px-8 py-2 md:px-20 md:py-12 h-[100vh] w-[100wh]">{children}</div> 
+        <div className="bg-black px-8 py-2 md:px-20 md:py-12 lg:h-[100vh] ">{children}</div> 
     )
 }
