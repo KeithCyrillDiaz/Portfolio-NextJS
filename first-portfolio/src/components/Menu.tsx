c25097950ba715f09f6abf29f88b36c196d7ad2f
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 17:40:34 +0800

    modified file to move the ismobile hook to custom hook

--- first-portfolio/src/components/Menu.tsx
+++ first-portfolio/src/components/Menu.tsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, {forwardRef} from "react";
 import { AboutSVG, HamburgerMenuSVG, HomeSVG, ProjectSVG } from "./Icons";
 import { HeaderDivider } from "./Header";
 import { useRouter } from "next/navigation";
@@ -21,22 +21,23 @@ type MenuProps = {
     onClick: () => void;
     currentPage: "Home" | "About" | "Projects";
 }
-export const Menu: React.FC<MenuProps> = ({onClick, currentPage}) => {
+export const Menu = forwardRef<HTMLDivElement, MenuProps>(({ onClick, currentPage }, ref) => {
     const router = useRouter();
-    return(
-        <div className="bg-defaultGray h-[100vh] w-[60%] left-0 top-0 pt-5 text-white absolute">
+    return (
+        <div ref={ref} className="bg-defaultGray h-[100vh] w-[60%] left-0 top-0 pt-5 text-white absolute">
             <HeaderDivider className="mx-4 py-[15.5px]">
                 <button onClick={onClick} className="font-montserrat font-extrabold text-[19px] mb-[-12px]">Menu</button>
             </HeaderDivider>
             <div className="my-2 flex flex-col space-y-2">
-                <MenuButton focus = {currentPage === "Home"} onClick={() => router.push('/')}>Home</MenuButton>
-                <MenuButton focus = {currentPage === "About"} onClick={() => router.push('/about')}>About</MenuButton>
-                <MenuButton focus = {currentPage === "Projects"} onClick={() => router.push('/project')}>Project</MenuButton>
+                <MenuButton focus={currentPage === "Home"} onClick={() => router.push('/')}>Home</MenuButton>
+                <MenuButton focus={currentPage === "About"} onClick={() => router.push('/about')}>About</MenuButton>
+                <MenuButton focus={currentPage === "Projects"} onClick={() => router.push('/project')}>Project</MenuButton>
             </div>
-          
         </div>
-    )
-}
+    );
+});
+
+Menu.displayName = "Menu";
 
 type MenuButtonProps = {
     children: React.ReactNode;
