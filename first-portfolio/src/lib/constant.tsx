b30ad9357d6fc02d5f0cc120a7ff9ead38cac96a
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 23:24:03 +0800

    add constants for all paragraphs and const data

---
+++ first-portfolio/src/lib/constant.tsx
@@ -0,0 +1,37 @@
+export const About = {
+    PersonalInformation: {
+        title: "Keith Cyrill A. Diaz",
+        firstParagpraph: [
+            "Hi there! I'm Keith, a fresh graduate passionate about building", 
+            " mobile", 
+            " and", 
+            " web",
+            " applications, with a current focus on JavaScript and Typescript."
+        ],
+        contactNumber: "+63 976-532-9159",
+        email: "keithalbarinadiaz0317@gmail.com",
+        address: "33 Julio’s Compound Pulang Lupa Uno Las Pinas City",
+        lastParagraph: "I was first exposed to technology at age four, which ignited my passion for technologies. This interest grew during my college years, leading me to pursue a degree in Information Systems. Throughout my studies, I gained hands-on experience in full-stack development during my thesis project. I thrive in collaborative environments where I can contribute to meaningful projects and grow both personally and professionally while aiming to make a positive impact in the tech industry."
+    },
+
+    Education: {
+        courseAndGPA: ["Bachelor of Science in Information System: GPA: ", "1.567"],
+        school: "Technological University of the Philippines",
+        bestThesisCOS: ["Awarded" , "Best Thesis", "in College of Science Department"],
+        cumLaude: ["Awarded", "Cum Laude"],
+        bestThesisCourse: ["Awarded" , "Best Thesis", "in Information System "],
+    },
+
+    Experience: {
+        date: "March-June 2024",
+        company: "Service Economy Applications INC – Alabang, Muntinlupa",
+        role: "Software Developer Intern",
+        bullets: [
+            "Developed and maintained backend services using Node.js and Express with TypeScript, implementing RESTful APIs, token-based authentication, and middleware for enhanced security and functionality.",
+
+            "Utilized React with TypeScript, integrating React Hooks and custom components to build dynamic and responsive web applications."
+        ]
+    }
+
+}
+
