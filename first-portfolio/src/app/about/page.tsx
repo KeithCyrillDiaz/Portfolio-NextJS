3e3eb349f570aeb5116e3e73d78fa53cbc46c47b
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 23:27:24 +0800

    modified files for about page

--- first-portfolio/src/app/about/page.tsx
+++ first-portfolio/src/app/about/page.tsx
@@ -1,9 +1,19 @@
+import { AboutMe } from '@/components/About/AboutMe'
+import { Education } from '@/components/About/Education'
+import { BackgroundColor } from '@/components/BackgroundColor'
+import { Container } from '@/components/Container'
+import { Header } from '@/components/Header'
 import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
-    </div>
+    <BackgroundColor className='pb-12'>
+       <Container>
+        <Header focus='About'/>
+        <AboutMe/>
+        <Education/>
+      </Container>
+    </BackgroundColor>
+   
   )
 }
---
+++ first-portfolio/src/components/About/PersonalInformation.tsx
@@ -0,0 +1,82 @@
+import { ChildrenClassNameProps } from "@/lib/types";
+import React from "react";
+import { Title } from "../Home/Information";
+import { About } from "@/lib/constant";
+import { AddressSVG, EmailSVG, PhoneSVG } from "../Icons";
+
+
+const Paragraph: React.FC<ChildrenClassNameProps> = ({
+    children,
+    className
+}) => {
+    return(
+        <div className={`${className} font-montserrat font-light text-[12px] xl:text-[15px] 2xl:text-[19px] text-white text-justify`}>{children}</div>
+    )
+}
+
+type DetailsContainerProps = {
+    children: React.ReactNode;
+    className?: string;
+    icon: "email" | "contact" | "address";
+    title: "Contact Number" | "Email" | "Address"
+}
+
+const DetailsContainer: React.FC<DetailsContainerProps> = ({
+    children,
+    className,
+    icon,
+    title
+}) => {
+    return(
+        <div className={`${className} flex flex-row text-[12px] xl:text-[15px] 2xl:text-[19px] text-white items-center`}>
+                {icon === "email" ?  <EmailSVG/> : icon === "contact" ? <PhoneSVG/> : <AddressSVG/>}
+                <span className="font-bold mx-2">{title}: </span>
+                {children}
+        </div>
+    )
+}
+
+const PersonalDetails: React.FC= () => {
+    const {email, contactNumber, address} = About.PersonalInformation
+    return(
+        <div 
+        className="my-2 mx-4"
+        >   
+           <DetailsContainer icon="contact" title="Contact Number">{contactNumber}</DetailsContainer>
+           <DetailsContainer icon="email" title="Email">{email}</DetailsContainer>
+           <DetailsContainer icon="address" title="Address">{address}</DetailsContainer>
+
+        </div>
+    )
+}
+
+type PersonalInformationProps = {
+    className?: string;
+}
+
+export const PersonalInformation: React.FC<PersonalInformationProps> = ({
+    className
+}) => {
+    const{
+        firstParagpraph, 
+        title, 
+        lastParagraph,
+    } = About.PersonalInformation;
+    return (
+        <div className={`${className ? className : "w-[320px]"} flex flex-col slide-in-from-bottom`}>
+            <Title className="text-defaultGreen xl:text-[35px]">{title}</Title>
+            <Paragraph>
+                {firstParagpraph[0]}
+                <span className="text-defaultGreen font-semibold">{firstParagpraph[1]}</span>
+                {firstParagpraph[2]}
+                <span className="text-defaultGreen font-semibold">{firstParagpraph[3]}</span>
+                {firstParagpraph[4]}
+            </Paragraph>
+            <PersonalDetails/>
+            <Paragraph>{lastParagraph}</Paragraph>
+
+        </div>
+
+    )
+}
+
