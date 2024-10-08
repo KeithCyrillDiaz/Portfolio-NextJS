dbfc8ce628ca60b8b5b0c1e05265b96f860cf6f0
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 18:11:55 +0800

    added class name prop to dynamically add a classname if needed

--- first-portfolio/src/components/Container.tsx
+++ first-portfolio/src/components/Container.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 type ContainerProps = {
     children: React.ReactNode;
+    className: string
 }
 
-export const Container: React.FC<ContainerProps> = ({children}) => {
+export const Container: React.FC<ContainerProps> = ({children, className}) => {
     return(
-        <div className="bg-black px-8 py-2 md:px-20 md:py-12 lg:h-[100vh] ">{children}</div> 
+        <div className={`bg-black px-8 py-2 md:px-20 md:py-12 lg:h-[100vh] ${className}`}>{children}</div> 
     )
 }
