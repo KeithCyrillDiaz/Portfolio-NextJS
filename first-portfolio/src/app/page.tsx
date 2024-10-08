18eb0479f928ca84f7560f09fcc24a784c73ec61
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-08 23:26:54 +0800

    adjust file to make it better

--- first-portfolio/src/app/page.tsx
+++ first-portfolio/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { BackgroundColor } from "@/components/BackgroundColor";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
 import { HomePageContent } from "@/components/Home/HomaPageContent";
@@ -5,12 +6,12 @@ import { HomePageContent } from "@/components/Home/HomaPageContent";
 
 export default function Home() {
   return (
-    <div className="bg-black h-[100vh] sm:h-full">
-      <Container>
-          <Header focus="Home">Home</Header>
-          <HomePageContent/>
-      </Container>
-    </div>
-   
+      <BackgroundColor>
+         <Container>
+            <Header focus="Home"/>
+            <HomePageContent/>
+        </Container>
+      </BackgroundColor>
+
   );
 }
