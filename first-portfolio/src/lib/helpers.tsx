3a2ecf020bac5b7ea170b40df2baa78b6f68e2d3
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-14 16:44:18 +0800

    added orientationidentifier helper but did not use atm

--- first-portfolio/src/lib/helpers.tsx
+++ first-portfolio/src/lib/helpers.tsx
@@ -1,4 +1,6 @@
-import { TechnicalSkillTypes } from "./constant";
+import { TechnicalSkillTypes } from "./Constants/About";
+import { imageType } from "./Constants/Projects";
+
 
 
 export const isFirstHalf = (index: number, arrayLength: number): boolean => {
@@ -14,3 +16,60 @@ export const getSecondHalfItems = (index: number, items: TechnicalSkillTypes[] )
 export const validateYear = (year: number) => {
     return year > 2023;
 }
+const getImageDimensions = (imageUrl: string): Promise<{ width: number, height: number }> => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+  
+      img.onload = () => {
+        resolve({
+          width: img.naturalWidth,
+          height: img.naturalHeight
+        });
+      };
+  
+      img.onerror = (error) => {
+        reject(new Error("Failed to load image"));
+      };
+  
+      img.src = imageUrl;
+    });
+  }
+  
+  export const isImagePortrait = async (imageUrl: string): Promise<boolean> => {
+    try {
+        const {width, height} = await getImageDimensions(imageUrl)
+        return height > width
+    } catch (error) {
+        console.log("error: ", error)
+        return false
+    }
+  }
+
+  export const separatePortraitAndLandscapeImages = async (images: imageType[]) => {
+    try {
+
+        console.log(images)
+        let portraitUrls: string[] = [];
+        let landscapeUrls: string[] = [];
+
+        for(const imageData of images){
+            const {imagesURL} = imageData
+            for (const url of imagesURL){
+                const result = await isImagePortrait(url)
+                if(result){
+                    portraitUrls.push(url)
+                }
+                else landscapeUrls.push(url)
+            }
+        }
+
+        console.log("portrait: ", portraitUrls)
+        console.log("landscapeUrls: ", landscapeUrls)
+
+        //unfinished, not yet needed
+
+    } catch (error) {
+        console.log("", error)
+        return 
+    }
+  }
