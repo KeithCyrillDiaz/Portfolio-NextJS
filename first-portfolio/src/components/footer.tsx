0c2bb04e4922dad5d99d5af3408063d149828b75
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-14 22:45:57 +0800

    modified footer

--- first-portfolio/src/components/footer.tsx
+++ first-portfolio/src/components/footer.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import { HeaderDivider } from "./Header";
 
 
-export const Footer: React.FC = () => {
+export const Footer: React.FC<{focus: 'Home' | 'About' | 'Projects' }> = ({focus}) => {
     return (
-        <HeaderDivider className="border-t-2 border-t-defaultGreen mt-7 pb-7 text-white items-center justify-center font-montserrat font-bold text-center">
-            That&#39;s All for Now
-        </HeaderDivider>
+        <>
+        {focus === "About" ? (
+            <HeaderDivider className="border-t-2 border-t-defaultGreen mt-7 pb-7 text-white items-center justify-center font-montserrat font-light italic text-[12px] xl:text-[17px] text-center">
+                1 Corinthians 10:31 (KJV) : <span className="italic">Whether therefore ye eat, or drink, or whatsoever ye do, do all to the glory of God.</span>
+                That&#39;s All for Now
+            </HeaderDivider>
+           
+        ) : (
+            <HeaderDivider className="border-t-2 border-t-defaultGreen mt-7 pb-7 text-white items-center justify-center font-montserrat font-bold text-center">
+                That&#39;s All for Now
+            </HeaderDivider>
+        )}
+        </>
+      
+      
     )
 }
